ba9e48128e2f1c725e4ee7b01c1e2a76089e5d0f
Tiago Henrique <tiagohs.dev@gmail.com>
2024-10-03 00:34:55 -0300

    [chore]: remove a constante ITEMS_PIPELINE do arquivo de configurações globais

--- mutuca/settings.py
+++ mutuca/settings.py
@@ -69,9 +69,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    "mutuca.pipelines.GoogleDriveLoadPDF": 1,
-}
+# ITEM_PIPELINES = {
+#     "mutuca.pipelines.GoogleDriveLoadPDF": 1,
+# }
 
 # FILES_STORE = ""
 
