46160e3e0d4ddc65f696ed001de72ae6efba153d
Tiago Henrique <tiago.henrique@picpay.com>
2024-10-19 12:04:46 -0300

    [feat]: inicio da implementação do crawler de obras publicas

--- scrapy/mutuca/mutuca/items/public_works_cityhall_items.py
+++ scrapy/mutuca/mutuca/items/public_works_cityhall_items.py
@@ -1,18 +1,16 @@
 import scrapy
 
-
 class CityHallPublicWorksItem(scrapy.Item):
-    modality_bidding_number = scrapy.Field()
-    project_description = scrapy.Field()
-    agreement = scrapy.Field()
-    contracted = scrapy.Field()
-    contract = scrapy.Field()
-    amendment = scrapy.Field()
-    expenses = scrapy.Field()
-    total_paid_amount = scrapy.Field()
-    status = scrapy.Field()
-    project_stage = scrapy.Field()
-    completion_percentage = scrapy.Field()
-    all_documents = scrapy.Field()
-    geo_coordinates = scrapy.Field()
-
+    numero_licitacao_modalidade = scrapy.Field()
+    descricao_projeto = scrapy.Field()  
+    convenio = scrapy.Field()  
+    contratado = scrapy.Field() 
+    contrato = scrapy.Field()  
+    aditivo = scrapy.Field()  
+    despesas = scrapy.Field()  
+    valor_total_pago = scrapy.Field()  
+    status = scrapy.Field()  
+    fase_projeto = scrapy.Field()  
+    percentual_conclusao = scrapy.Field() 
+    todos_documentos = scrapy.Field()  
+    coordenadas_geograficas = scrapy.Field() 
