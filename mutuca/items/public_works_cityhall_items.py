2b65c26f75843bde08f3fba47cc688096633dfa0
datafixer <datafixer.br@gmail.com>
2025-09-28 10:37:41 -0300

    [refact]: alteração da estrutura de itens para coleta de dados

--- mutuca/items/public_works_cityhall_items.py
+++ mutuca/items/public_works_cityhall_items.py
@@ -1,16 +1,13 @@
 import scrapy
 
+
 class CityHallPublicWorksItem(scrapy.Item):
-    numero_licitacao_modalidade = scrapy.Field()
-    descricao_projeto = scrapy.Field()  
-    convenio = scrapy.Field()  
-    contratado = scrapy.Field() 
-    contrato = scrapy.Field()  
-    aditivo = scrapy.Field()  
-    despesas = scrapy.Field()  
-    valor_total_pago = scrapy.Field()  
-    status = scrapy.Field()  
-    fase_projeto = scrapy.Field()  
-    percentual_conclusao = scrapy.Field() 
-    todos_documentos = scrapy.Field()  
-    coordenadas_geograficas = scrapy.Field() 
+    general_info = scrapy.Field()
+    contract = scrapy.Field()
+    contracted = scrapy.Field()
+    fiscal_year_expenditures = scrapy.Field()
+    contract_amendment = scrapy.Field()
+    cumulative_amount_paid = scrapy.Field()
+    all_documents = scrapy.Field()
+    work_location = scrapy.Field()
+
