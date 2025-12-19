f0f478a6943de3fb47a480466d99ec8a69aae225
datafixer <datafixer.br@gmail.com>
2025-12-19 18:34:49 -0300

    [chore]: define o formato de saída como json em settings

--- scrapy/mutuca/mutuca/settings.py
+++ scrapy/mutuca/mutuca/settings.py
@@ -75,7 +75,7 @@ DOWNLOADER_MIDDLEWARES = {
 
 # FILES_STORE = ""
 
-# FEEDS = {"parliamentary_allowance_metadata.csv": {"format": "csv", "overwrite": False}}
+# FEEDS = {"public_works.json": {"format": "json"}}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
