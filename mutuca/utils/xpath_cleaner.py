5a2a80511da20e67f87ecbb50250f696b028a5c4
datafixer <datafixer.br@gmail.com>
2025-12-22 14:31:49 -0300

    [chore]: apaga os comentários desnecessários

--- mutuca/utils/xpath_cleaner.py
+++ mutuca/utils/xpath_cleaner.py
@@ -6,6 +6,21 @@ class XPathCleaner:
     """Utilitário para limpeza de dados extraídos via XPath"""
 
     @staticmethod
+    # def clean_xpath_results(self, values: List[str]) -> List[str]:
+    #     cleaned_values = []
+    #     for item in values:
+    #         # Remove tags <div> e </div>
+    #         text = re.sub(r"<div[^>]*>", "", item)
+    #         text = re.sub(r"</div>", "", text)
+    #         # Remove quebras de linha e espaços extras
+    #         text = text.replace("\n", "").strip()
+    #         # Remove múltiplos espaços internos
+    #         text = re.sub(r"\s+", " ", text)
+    #         # Adiciona apenas se não for vazio
+    #         if text:
+    #             cleaned_values.append(text)
+
+    #     return cleaned_values
     def clean_html_text(values: List[str]) -> List[str]:
         return [
             cleaned
